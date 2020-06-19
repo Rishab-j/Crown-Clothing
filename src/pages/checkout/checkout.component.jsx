a696232a3b79e5f3d247a524a392a67136b265c5
Rishab Jain <rjrishabh.jain5@gmail.com>
2020-06-19 16:52:05 +0530

    added stripe payment feature and changed title and logo of the app

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutComponent from '../../components/stripe-button/stripe-button.component';
 
 import { selectCartItems,selectCartTotal } from '../../redux/cart/cart.selectors';
 
@@ -34,10 +35,10 @@ const CheckoutPage = ({cartItems,total}) => (
                 )
                 )
         }
-
         <div className='total'>
             <span>TOTAL : ${total}</span>
         </div>
+        <StripeCheckoutComponent price={total}/>
     </div>
 )
 
