26a107b7852af29e68f5f232c7977940cdbc6fd8
Rishab Jain <rjrishabh.jain5@gmail.com>
2020-06-20 20:59:56 +0530

    updating code to leverage containers for collections-overview and collection-page components

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -12,7 +12,7 @@ import {
 } from './collection.styles';
 
 const CollectionPage = ({ collection }) => {
-  const {items ,title } = collection;
+  const { title, items } = collection;
   return (
     <CollectionPageContainer>
       <CollectionTitle>{title}</CollectionTitle>
