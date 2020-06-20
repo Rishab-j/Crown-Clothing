55c2aeae26680bac1bef30ac8962d54519c16099
Rishab Jain <rjrishabh.jain5@gmail.com>
2020-06-20 20:09:24 +0530

    added redux thunk to the app and tried to fix the last bug but its still not working on my firebase config

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,17 @@ import 'firebase/auth';
 //     measurementId: "G-R7NCPGZKKT"
 //   };
 
+// const config = {
+//   apiKey: "AIzaSyAFDsR5agXoYM8o8NKuZ18-PnTelhjBmaM",
+//   authDomain: "crwn-db-d5bee.firebaseapp.com",
+//   databaseURL: "https://crwn-db-d5bee.firebaseio.com",
+//   projectId: "crwn-db-d5bee",
+//   storageBucket: "crwn-db-d5bee.appspot.com",
+//   messagingSenderId: "972846238953",
+//   appId: "1:972846238953:web:adda29ce8d280942773478",
+//   measurementId: "G-R7NCPGZKKT"
+// };
+
 const config = {
   apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
   authDomain: 'crwn-db.firebaseapp.com',
